03031db6a956e16f6f4b50c2e7d18a2979070ec1
strechpac <makaveli4real60@yahoo.com>
2023-10-16 04:50:26 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * rest_to_98 - a function that takes a pointer to an int as parameter
+ * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it point to to 98
  * @n: input
  * Return: n
