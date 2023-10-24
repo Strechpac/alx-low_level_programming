f66f9c72a9ad3128ec0754daec2c5e58ca679414
strechpac <makaveli4real60@yahoo.com>
2023-10-24 01:42:53 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -4,8 +4,9 @@
 /**
  * _putchar - write the character c to stdout
  * @c: The character to print
+ *
  * Return: On success 1.
- * on error, 1 is returned, and errno is appropriately.
+ * on error, -1 is returned, and errno is appropriately.
  */
 int _putchar(cha c)
 {
