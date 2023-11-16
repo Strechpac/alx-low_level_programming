2a7bdcfeedcd96ba90c8086e9c867bd5c7e1f309
strechpac <makaveli4real60@yahoo.com>
2023-11-16 08:10:10 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include "lists.h"
 
-/**
- * before_main - funcion that prints a specific string before the main,
- * function in executed.
- *
- * Return: no return.
+/*
+ * File: 100-first.c
+ * Auth: Brennan D Baraban
  */
 
+#include <stdio.h>
 
-void before_main(void)
+void __attribute__((constructor)) hare(void);
+
+/**
+ * hare - Prints a string before the
+ *        main function is executed.
+ */
+void hare(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
